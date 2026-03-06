ebc231cf96dfa58a9f8c688dba924410f73f8bf2
Adigwe Dennis <talk2dennis>
2026-03-06 22:19:37 +0100

    added admin functions

--- afms-backend/src/modules/auths/auth.routes.js
+++ afms-backend/src/modules/auths/auth.routes.js
@@ -5,9 +5,15 @@ import {
   forgotPassword,
   resetPassword,
   getCurrentUser,
-  updateUserDetails
+  updateUserDetails,
+  adminLogin,
+  getAllUsers,
+  deleteUser,
+  updateUserRole,
+  getStatistics
 } from './auth.controller.js'
 import { protect } from '../../middleware/auth.middleware.js'
+import protectAdmin from '../../middleware/role.middleware.js'
 
 const authRouter = express.Router()
 
@@ -18,4 +24,10 @@ authRouter.get('/me', protect, getCurrentUser)
 authRouter.post('/forgot-password', forgotPassword)
 authRouter.post('/reset-password/:token', resetPassword)
 authRouter.put('/me', protect, updateUserDetails)
+// admin routes
+authRouter.post('/admin/login', adminLogin)
+authRouter.get('/admin/users', protect, getAllUsers)
+authRouter.delete('/admin/users/:id', protect, protectAdmin, deleteUser)
+authRouter.put('/admin/users/:id/role', protect, protectAdmin, updateUserRole)
+authRouter.get('/admin/statistics', protect, protectAdmin, getStatistics)
 export default authRouter
